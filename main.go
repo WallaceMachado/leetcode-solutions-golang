717d61f7eb13a74012ca39464549aca497b1f01c
wallace machado <wallacemachado@ntopus.com>
2022-08-31 21:51:49 -0300

    380. Insert Delete GetRandom O(1)

--- main.go
+++ main.go
@@ -1,1 +1,105 @@
-package leetcode
+package main
+
+/*
+380. Insert Delete GetRandom O(1)
+Medium
+
+5931
+
+313
+
+Add to List
+
+Share
+Implement the RandomizedSet class:
+
+RandomizedSet() Initializes the RandomizedSet object.
+bool insert(int val) Inserts an item val into the set if not present. Returns true if the item was not present, false otherwise.
+bool remove(int val) Removes an item val from the set if present. Returns true if the item was present, false otherwise.
+int getRandom() Returns a random element from the current set of elements (it's guaranteed that at least one element exists when this method is called). Each element must have the same probability of being returned.
+You must implement the functions of the class such that each function works in average O(1) time complexity.
+
+
+
+Example 1:
+
+Input
+["RandomizedSet", "insert", "remove", "insert", "getRandom", "remove", "insert", "getRandom"]
+[[], [1], [2], [2], [], [1], [2], []]
+Output
+[null, true, false, true, 2, true, false, 2]
+
+Explanation
+RandomizedSet randomizedSet = new RandomizedSet();
+randomizedSet.insert(1); // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+randomizedSet.remove(2); // Returns false as 2 does not exist in the set.
+randomizedSet.insert(2); // Inserts 2 to the set, returns true. Set now contains [1,2].
+randomizedSet.getRandom(); // getRandom() should return either 1 or 2 randomly.
+randomizedSet.remove(1); // Removes 1 from the set, returns true. Set now contains [2].
+randomizedSet.insert(2); // 2 was already in the set, so return false.
+randomizedSet.getRandom(); // Since 2 is the only number in the set, getRandom() will always return 2.
+
+
+Constraints:
+
+-231 <= val <= 231 - 1
+At most 2 * 105 calls will be made to insert, remove, and getRandom.
+There will be at least one element in the data structure when getRandom is called.
+*/
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+type RandomizedSet struct {
+	Values []int
+}
+
+func Constructor() RandomizedSet {
+	var values []int
+	return RandomizedSet{Values: values}
+}
+
+func (this *RandomizedSet) Insert(val int) bool {
+	fmt.Println("this.Values ", this.Values)
+	for _, v := range this.Values {
+		fmt.Println("v ", v)
+		if v == val {
+			fmt.Println("v ", val)
+			return false
+		}
+	}
+	this.Values = append(this.Values, val)
+	fmt.Println("this.Values ", this.Values)
+	return true
+}
+
+func (this *RandomizedSet) Remove(val int) bool {
+	existsVal := false
+	for i, v := range this.Values {
+		if v == val {
+			this.Values = append(this.Values[:i], this.Values[i+1:]...)
+			existsVal = true
+		}
+	}
+	return existsVal
+}
+
+func (this *RandomizedSet) GetRandom() int {
+	num := rand.Intn(len(this.Values))
+	return this.Values[num]
+}
+
+func main() {
+	// 	["RandomizedSet","remove","remove","insert","getRandom","remove","insert"]
+	//[[],[0],[0],[0],[],[0],[0]]
+	r := Constructor()
+	r.Remove(0)
+	r.Remove(0)
+	r.Insert(0)
+	r.GetRandom()
+	r.Remove(0)
+	r.Insert(0)
+
+}
