e0fde98b682cd255cd746cb5439467cf3e9c17c4
wallace machado <wallacemachado@ntopus.com>
2022-09-17 09:33:27 -0300

    Jewels and Stones

---
+++ solutions/numJewelsInStones.go
@@ -0,0 +1,46 @@
+package solutions
+
+/*
+771. Jewels and Stones
+Easy
+
+3924
+
+505
+
+Add to List
+
+Share
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+
+
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: jewels = "z", stones = "ZZ"
+Output: 0
+
+
+Constraints:
+
+1 <= jewels.length, stones.length <= 50
+jewels and stones consist of only English letters.
+All the characters of jewels are unique.
+*/
+func numJewelsInStones(jewels string, stones string) int {
+	output := 0
+	for i := 0; i < len(stones); i++ {
+		for j := 0; j < len(jewels); j++ {
+			if stones[i] == jewels[j] {
+				output++
+			}
+		}
+	}
+	return output
+}
